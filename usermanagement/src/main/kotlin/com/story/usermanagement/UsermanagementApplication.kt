54cc51edd56cfbcc1df6bc870e12ed62866c8830
areeba-fatima-4 <areeba.fatima@10pearls.com>
2023-07-23 15:41:26 +0500

    Initial commit

---
+++ usermanagement/src/main/kotlin/com/story/usermanagement/UsermanagementApplication.kt
@@ -0,0 +1,11 @@
+package com.story.usermanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsermanagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<UsermanagementApplication>(*args)
+}
---
+++ usermanagement/src/test/kotlin/com/story/usermanagement/UsermanagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.story.usermanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UsermanagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
